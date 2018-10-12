7fcc2024eae21c1edcb10a727c4b1e27138bfd15
kungfunk <kung.funk@gmail.com>
2018-10-12 14:19:17 +0200

    Added Log model methods for ADR.

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -2,9 +2,23 @@
 namespace Models;
 
 use Illuminate\Database\Eloquent\Model as Model;
+use Illuminate\Database\Eloquent\Builder as Query;
 
 class Log extends Model
 {
+    const LEVEL_INFO = 'info';
+    const LEVEL_NOTICE = 'notice';
+    const LEVEL_WARNING = 'warning';
+    const LEVEL_ERROR = 'error';
+
+    const DEFAULT_ORDER_FIELD = 'created_at';
+    const DEFAULT_ORDER_DIRECTION = 'desc';
+
+    const ORDER_FIELD_WHITELIST = [
+        'created_at',
+        'level',
+    ];
+
     const UPDATED_AT = null;
 
     protected $fillable = [
@@ -15,8 +29,24 @@ class Log extends Model
         'extra'
     ];
 
+    protected $casts = [
+        'context' => 'array',
+        'extra' => 'array'
+    ];
+
     public function user()
     {
         return $this->belongsTo('Models\User');
+    }
+
+    public function scopeFilters(Query $query, array $filters)
+    {
+        foreach ($filters as $name => $value) {
+            if (!empty($value)) {
+                $query->where($name, $value);
+            }
+        }
+
+        return $query;
     }
 }
